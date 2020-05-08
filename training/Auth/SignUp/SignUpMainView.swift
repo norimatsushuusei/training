820e992cd0fb442ac8a8f65c2742c3596afd0a33
norimatsushuusei <s.norimatsu@a-sw.jp>
2020-05-08 14:52:55 +0900

    fix training

---
+++ training/Auth/SignUp/SignUpMainView.swift
@@ -0,0 +1,40 @@
+//
+//  SignUpMainView.swift
+//  training
+//
+//  Created by ASW-研修４ on 2020/05/08.
+//  Copyright © 2020 ASW-研修４. All rights reserved.
+//
+
+import UIKit
+import PGFramework
+protocol SignUpMainViewDelegate: NSObjectProtocol{
+    func touchedSignUpButton()
+}
+extension SignUpMainViewDelegate {
+}
+// MARK: - Property
+class SignUpMainView: BaseView {
+    weak var delegate: SignUpMainViewDelegate? = nil
+    //Outlet
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    //Action
+    @IBAction func touchedSignUpButton(_ sender: UIButton) {
+        if let delegate = delegate{delegate.touchedSignUpButton()}
+    }
+}
+// MARK: - Life cycle
+extension SignUpMainView {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
+// MARK: - Protocol
+extension SignUpMainView {
+}
+// MARK: - method
+extension SignUpMainView {
+}
